f19cc2193a2a0cbc2ab8cdf863803281c7f34296
Flavio Macedo <fhenrique609@gmail.com>
2025-02-26 11:13:03 -0300

    Ensure cart total is shown

--- src/app/[slug]/menu/components/cart-product-item.tsx
+++ src/app/[slug]/menu/components/cart-product-item.tsx
@@ -14,7 +14,7 @@ interface CartItemProps {
 const CartProductItem = ({ product }: CartItemProps) => {
     const { decreaseProductQuantity, increaseProductQuantity, removeProduct } = useContext(CartContext);
     return (
-        <div className="flex items-center justify-between">
+        <div className="flex items-center justify-between mb-4">
             {/* ESQUERDA */}
             <div className="flex items-center gap-3">
                 <div className="relative h-20 w-20 rounded-xl bg-gray-100">
